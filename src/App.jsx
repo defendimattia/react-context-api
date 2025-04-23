80cc5764854730b8f2ad694dc1cd403d67c1866b
defendimattia <defendi.mattia92@gmail.com>
2025-04-23 14:40:31 +0200

    add: old repo structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import AboutUs from "./pages/AboutUs"
+import PostList from "./pages/PostList"
+import PostPage from "./pages/PostPage"
+import DefaultLayout from "./layouts/DefaultLayout"
+import "./App.css"
+
 function App() {
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+
+          <Route element={<DefaultLayout />}>
+            <Route index element={<Homepage />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+
+            <Route path="/postlist">
+              <Route index element={<PostList />} />
+              <Route path=":id" element={<PostPage />} />
+            </Route>
+
+            <Route path="*" element={<h1>Pagina non trovata!</h1>} />
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
